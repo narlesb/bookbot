df31184af1fd65a330c1d1a1ca18fd1a163ee069
minose <minose@live.ca>
2025-03-05 11:52:32 -0800

    word count functionality

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def main():
+    book_path = 'books/frankenstein.txt'
+    text = get_book_text(book_path)
+    num_words = wordcount(text)
+    print (f"{num_words} words found in the document")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def wordcount(text):
+    words = text.split()
+    return len(words)
+
+    
+main()
+
+    
